8df2d673da957c0fbfcb80b601f9b0e269301a17
Gabrielle Guarino <gguarino@umich.edu>
2017-04-11 21:25:08 -0400

    new stuff

--- image.py
+++ image.py
@@ -11,16 +11,47 @@ from serial import Serial
 from collections import namedtuple
 import logging
 import numpy
+import time
 
+# create word drawing type, andrews letters and pixel letters
+# be able to specify which marker color for letters and for line drawings
+# largest size is 450
 
 app = Flask(__name__)
 
+ser = 0
+loc = namedtuple("loc", "x3 x2 x1 x0 y3 y2 y1 y0 servo_num")
+locations = []
+
+#########################
+# SERVO INFO
+#########################
+
+# servo 1
+# black
+
+# servo2
+# red
+
+# servo3
+# green
+
+# servo4
+# blue
 
 #########################
 # SERIAL
 #########################
 
-ser = 0
+def test_90_loop():
+  start = time.time()
+  x = 0
+  for i in range(0,90000):
+    x += 1
+  end = time.time()
+  print (end - start)
+  return
+
 
 def init_serial():
   global ser
@@ -48,21 +79,36 @@ def send_points():
   while "".join(readline()) != "<<send>>":
     # waiting
     pass
-  for x in locations:
-    ser.write(str(x.x))
-    ser.write(str(x.y))
+  for i in locations:
+    ser.write(str(i.x3))
+    ser.write(str(i.x2))
+    ser.write(str(i.x1))
+    ser.write(str(i.x0))
+    ser.write(str(i.y3))
+    ser.write(str(i.y2))
+    ser.write(str(i.y1))
+    ser.write(str(i.y0))
+    ser.write(str("0"))
+    ser.write(str("0"))
+    ser.write(str("0"))
+    ser.write(str(i.servo_num))
+    while "".join(readline()) != "<<send>>":
+      # waiting
+      pass
   ser.write("\n<<end>>\n")
 
 
 #########################
 # IMAGE PROCESSING
 #########################
-loc = namedtuple("loc", "x y")
-locations = []
 
-def print_pixels(img):
-  height = len(img[:][:][:])
-  width = len(img[0][:][:])
+bit_mask_index_3 = 0xF000
+bit_mask_index_2 = 0x0F00
+bit_mask_index_1 = 0x00F0
+bit_mask_index_0 = 0x000F
+
+# only prints black/white
+def print_pixels(img, height, width):
   for row in range(1,height):
     for col in range(1,width):
       if (img[row][col][0] == 0):
@@ -71,11 +117,11 @@ def print_pixels(img):
         print "X",
     print " "
 
-def canny(img):
-  width = len(img[0][:])
+# canny edge detection
+# for line drawing
+def canny(img, width):
   img = misc.imresize(img, 300.0/width)
   return feature.canny(img, sigma=3)
-
 
 def sobel(img):
   img_sx = ndimage.sobel(img, axis=0, mode='constant')
@@ -85,53 +131,147 @@ def sobel(img):
   img_resize = misc.imresize(img_mag, 0.25)
   return img_resize
 
+# if r is largest, use red marker
+# if g is largest, use green marker
+# if b is largest, use blue marker
+# if its a grayscale color...
+# less than half of 256, use white, aka no marker
+# greater than half of 256, use black
+def find_closest(r, g, b):
+  if (r > g and r > b):
+    return 2 # red
+  if (g > r and g > b):
+    return 3 # green
+  if (b > r and b > g):
+    return 4 # blue
+  if (r == g and r == b):
+    if (r < 128):
+      return 0 # white, nothing
+    else:
+      return 1 # black
 
+# create file of points
+# for debugging
 def create_file_of_points(img, filename):
   text_file = open(filename, "w")
   global locations
-  for x in locations:
-    to_write = str(x.x) + " " + str(x.y) + "\n"
+  for i in locations:
+    to_write =  str(i.x3) + " " + \
+                str(i.x2) + " " + \
+                str(i.x1) + " " + \
+                str(i.x0) + " " + \
+                str(i.y3) + " " + \
+                str(i.y2) + " " + \
+                str(i.y1) + " " + \
+                str(i.y0) + " " + \
+                str(i.servo_num)
     text_file.write("%s" % to_write)
   text_file.close()
 
-def create_array_of_points(img):
+# create array of points
+# to send over serial connection
+# line drawing, same marker whole time
+def create_array_of_points_line(img, height, width, servo_num):
   global locations
-  width = len(img[0][:])
-  height = len(img[:][:])
   for r in range(0, height):
     for c in range(0, width):
       if (img[r][c] == True):
-        l = loc(str(c), str(r))
+        c3 = c & bit_mask_index_3
+        c2 = c & bit_mask_index_2
+        c1 = c & bit_mask_index_1
+        c0 = c & bit_mask_index_0
+        r3 = r & bit_mask_index_3
+        r2 = r & bit_mask_index_2
+        r1 = r & bit_mask_index_1
+        r0 = r & bit_mask_index_0
+        l = loc(str(c3), str(c2), str(c1), str(c0), \
+                str(r3), str(r2), str(r1), str(r0), \
+                str(servo_num))
         locations.append(l)
 
+def sort():
+  # sort the locations array
+  # black first
+  # red second
+  # green third
+  # blue last
+  black = []
+  red = []
+  green = []
+  blue = []
+  new_locations = []
+  for i in locations:
+    if (i.servo_num == 1):
+      black.append(i)
+    elif (i.servo_num == 2):
+      red.append(i)
+    elif (i.servo_num == 3):
+      green.append(i)
+    elif (i.servo_num == 4):
+      blue.append(i)
+    while (black):
+      add = black.pop(0)
+      new_locations.append(add)
+    while (red):
+      add = red.pop(0)
+      new_locations.append(add)
+    while (green):
+      add = blue.pop(0)
+      new_locations.append(add)
+    while (blue):
+      add = blue.pop(0)
+      new_locations.append(add)
+  return new_locations
+
+# create array of points
+# to send over serial connection
+# dot drawing, different marker depending on color
+def create_array_of_points_color(img, height, width):
+  for r in range(0, height):
+    for c in range(0, width):
+      c3 = c & bit_mask_index_3
+      c2 = c & bit_mask_index_2
+      c1 = c & bit_mask_index_1
+      c0 = c & bit_mask_index_0
+      r3 = r & bit_mask_index_3
+      r2 = r & bit_mask_index_2
+      r1 = r & bit_mask_index_1
+      r0 = r & bit_mask_index_0
+      servo_num = find_closest(img[r][c][0], img[r][c][1], img[r][c][0])
+      if (servo_num != 0):
+        l = loc(str(c3), str(c2), str(c1), str(c0), \
+                str(r3), str(r2), str(r1), str(r0), \
+                str(servo_num))
+        locations.append(l)
+
+def resize(img, height, width):
+  if (width > 450):
+    img = misc.imresize(img, 450.0/width)
+    height = len(img[:][:][:])
+  if (height > 450):
+    img = misc.imresize(img, 450.0/height)
+  return img
+
+def type1(img, height, width):
+  # line drawing
+  init_serial()
+  new_img = canny(img, width)
+  create_array_of_points_line(new_img, height, width, 1) # always black line
+  send_points()
 
 
+def type2(img, height, width):
+  # dot drawing
+  init_serial()
+  new_img = resize(img)
+  create_array_of_points_color(new_img, height, width)
+  locations = sort()
+  send_points()
 
-def edge_detection_test():
-  image1 = ndimage.imread('images/test1.jpg', mode="RGB")
-  image2 = ndimage.imread('images/test2.jpg', mode="RGB")
-  image3 = ndimage.imread('images/test3.jpg', mode="RGB")
-  image4 = ndimage.imread('images/test4.jpg', mode="RGB")
-  
-  image1_grey = numpy.dot(image1[...,:3],[0.3, 0.6, 0.1])
-  image2_grey = numpy.dot(image2[...,:3],[0.3, 0.6, 0.1])
-  image3_grey = numpy.dot(image3[...,:3],[0.3, 0.6, 0.1])
-  image4_grey = numpy.dot(image4[...,:3],[0.3, 0.6, 0.1])
-
-  image1_c_r = canny(image1_grey)
-  image2_c_r = canny(image2_grey)
-  image3_c_r = canny(image3_grey)
-  image4_c_r = canny(image4_grey)
-  
-  create_file_of_points(image1_c_r, "output1.txt")
-  create_file_of_points(image1_c_r, "output2.txt")
-  create_file_of_points(image1_c_r, "output3.txt")
-  create_file_of_points(image1_c_r, "output4.txt")
-
-  misc.imsave('output_images/test1_canny.jpg', image1_c_r)
-  misc.imsave('output_images/test2_canny.jpg', image2_c_r)
-  misc.imsave('output_images/test3_canny.jpg', image3_c_r)
-  misc.imsave('output_images/test4_canny.jpg', image4_c_r)
+def type3(words):
+  # word drawing
+  init_serial()
+  create_array_of_alpha()
 
 
 
@@ -141,11 +281,13 @@ def edge_detection_test():
 #########################
 
 @app.route('/type1', methods=['GET', 'POST'])
+# line drawing
 def type1_start():
   image = request.files.get('image-file', '')
   img = ndimage.imread(image)
   height = len(img[:][:][:])
   width = len(img[0][:][:])
+  type1(img, height, width)
   return "done"
 
 @app.route('/type2', methods=['GET', 'POST'])
@@ -154,7 +296,13 @@ def type2_start():
   img = ndimage.imread(image)
   height = len(img[:][:][:])
   width = len(img[0][:][:])
+  type2(img, height, width)
   return "done"
+
+@app.route()
+def type3_start():
+  words = request.args['data']
+  type3(words)
 
 
 #########################
@@ -171,5 +319,4 @@ def favicon():
   return send_from_directory('Documents/EECS373/final_proj/images', 'test.png')
 
 if __name__ == '__main__':
-  #app.run(debug=True)
-  edge_detection_test()
+  app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,43 +1,65 @@
 function get_input() {
   var formdata = new FormData($('#upload')[0]);
-  if ($('#draw_type1').checked && $('#draw_type2').checked)
+  if ($('#draw_type1').checked ^ $('#draw_type2').checked ^ $('#draw_type3').checked)
   {
-    alert("please only select one drawing style");
+    if ($('#draw_type1').checked)
+    {
+      $.ajax({
+             url: "/type1",
+             type: "POST",
+             data: formdata,
+             processData: false,
+             contentType: false,
+             cache: false,
+             async: false,
+             complete: function (data) {
+             console.log('complete');
+             },
+             error: function (data) {
+             console.log('error');
+             }
+             });
+    }
+    else if ($('#draw_type2').checked)
+    {
+      $.ajax({
+             url: "/type2",
+             type: "POST",
+             data: formdata,
+             processData: false,
+             contentType: false,
+             cache: false,
+             async: false,
+             complete: function (data) {
+             console.log('complete');
+             },
+             error: function (data) {
+             console.log('error');
+             }
+             });
+    }
+    else if ($('#draw_type3').checked)
+    {
+      var words = $('#draw_type3_words').toString();
+      $.ajax({
+             url: "/type3",
+             type: "POST",
+             data: {data: words},
+             processData: false,
+             contentType: false,
+             cache: false,
+             async: false,
+             complete: function (data) {
+             console.log('complete');
+             },
+             error: function (data) {
+             console.log('error');
+             }
+             });
+    }
   }
-  else if ($('#draw_type1').checked)
+  else if (($('#draw_type1').checked + $('#draw_type2').checked + $('#draw_type3').checked) > 0)
   {
-    $.ajax({
-           url: "/type1",
-           type: "POST",
-           data: formdata,
-           processData: false,
-           contentType: false,
-           cache: false,
-           async: false,
-           complete: function (data) {
-           console.log('complete');
-           },
-           error: function (data) {
-           console.log('error');
-           }
-    });
-  }
-  else if ($('#draw_type2').checked)
-  {
-    $.ajax({
-           url: "/type2",
-           type: "POST",
-           data: formdata,
-           processData: false,
-           contentType: false,
-           cache: false,
-           async: false,
-           complete: function (data) {
-           console.log('complete');
-           },
-           error: function (data) {
-           console.log('error');
-           }
-    });
+    alert("please only select one.");
   }
 }
